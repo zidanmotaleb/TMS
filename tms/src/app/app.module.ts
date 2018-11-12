10db303f792c2f82effc79fbe6d54ab35ca3cef8
Mohamed <almashrgy@gmail.com>
2018-11-12 11:14:38 -0500

    invoice & report comp created

--- tms/src/app/app.module.ts
+++ tms/src/app/app.module.ts
@@ -2,24 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { TicketComponent } from './ticket/ticket.component';
-||||||| merged common ancestors
-=======
+import { InvoiceComponent } from './invoice/invoice.component';
+import { ReportComponent } from './report/report.component';
+
 import { LoginComponent } from './login/login.component';
->>>>>>> 560f070628c99b7fc2ac1c7c7ac6795b7557d5a7
+
 
 @NgModule({
   declarations: [
-<<<<<<< HEAD
     AppComponent,
-    TicketComponent
-||||||| merged common ancestors
-    AppComponent
-=======
-    AppComponent,
+    TicketComponent,
+    InvoiceComponent,
+    ReportComponent,
     LoginComponent
->>>>>>> 560f070628c99b7fc2ac1c7c7ac6795b7557d5a7
+
   ],
   imports: [
     BrowserModule
